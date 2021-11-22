57c598e59f8e8b4d42d1839ecf62962ff22a10e8
Dan Cherino <dan.cherino@doctorcareanywhere.com>
2021-11-22 12:15:02 +0000

    Testing loading script

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+module.exports = {
+  webpack: (config, env) => {
+    config.entry = {
+      main: "./src/index.js",
+      load: "./public/load-micro-frontend.js",
+    };
+    config.optimization.runtimeChunk = false;
+    config.optimization.splitChunks = {
+      cacheGroups: {
+        default: false,
+      },
+    };
+
+    config.output.filename = "static/js/[name].js";
+
+    config.plugins[5].options.filename = "static/css/[name].css";
+    config.plugins[5].options.moduleFilename = () => "static/css/main.css";
+    return config;
+  },
+};
---
+++ public/load-micro-frontend.js
@@ -0,0 +1,34 @@
+const microURL = "https://calm-pond-05e0f7e03.azurestaticapps.net/";
+
+function loadURL() {
+  //   console.log(process.env.NODE_ENV);
+  //   const host =
+  //     process.env.NODE_ENV === "development" ? "http://localhost:3000" : microURL;
+
+  const scriptId = `micro-frontend-script`;
+
+  const renderScript = () => {
+    window[`renderFrontend`]();
+  };
+
+  if (document.getElementById(scriptId)) {
+    renderScript();
+    return;
+  }
+  const host = "http://localhost:3000";
+  fetch(`${host}/asset-manifest.json`)
+    .then((res) => res.json())
+    .then((manifest) => {
+      const script = document.createElement("script");
+      script.id = scriptId;
+      script.crossOrigin = "";
+      script.src = `${host}${manifest.files["main.js"]}`;
+      document.querySelector("#root").parentElement.appendChild(script);
+      script.onload = () => {
+        renderScript();
+      };
+      
+    });
+}
+
+loadURL();
